1091223f51c01737b3a9af485a8e3bdc74f4eb04
Furkan Uyanık <51247701+furkanuyanik@users.noreply.github.com>
2019-06-18 11:41:43 +0300

    Update CustomerRepository.java

--- src/main/java/com/furkanuyanik/springdemo/repository/CustomerRepository.java
+++ src/main/java/com/furkanuyanik/springdemo/repository/CustomerRepository.java
@@ -1,9 +1,9 @@
-package com.furkanuyanik.springdemo.repository;
+package com.ttgint.customer.repository;
 
-import com.furkanuyanik.springdemo.model.Customer;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import com.ttgint.customer.model.Customer;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
 }
